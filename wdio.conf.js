bbaff107e40163aea43e617609bbb6f673b2ab0c
haula <hau-la@cybozu.com>
2020-12-01 14:12:01 +0700

    Update all

--- wdio.conf.js
+++ wdio.conf.js
@@ -108,14 +108,14 @@ exports.config = {
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 20000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
-    connectionRetryTimeout: 240000,
+    connectionRetryTimeout: 360000,
     //
     // Default request retries count
-    connectionRetryCount: 3,
+    connectionRetryCount: 2,
     //
     // Test runner services
     // Services take over a specific job you don't want to take care of. They enhance
